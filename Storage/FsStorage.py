406bbb0a36d0213d25c054e6b14145097615f3ac
Zofikar <pawel.chwalczyk@gmail.com>
2026-05-22 18:31:18 +0200

    Add logger to FsStorage

--- Storage/FsStorage.py
+++ Storage/FsStorage.py
@@ -1,9 +1,13 @@
+import logging
+
 from Protocol import T
 from Storage import IStorage
 from datetime import datetime
 import csv
 import os
 from pathlib import Path
+
+logger = logging.getLogger("FsStorage")
 
 class FsStorage(IStorage[T]):
     """Directly writes data to a specified filesystem path."""
@@ -12,14 +16,16 @@ class FsStorage(IStorage[T]):
         self.target_file_path = target_file_path
         self.storage_label = storage_label
 
-    def write(self, data: T) -> bool:
+    def write(self, data: T):
         payload = data.to_dict()
 
         file_exists = self.target_file_path.exists()
         self.target_file_path.parent.mkdir(parents=True, exist_ok=True)
 
-        with open(self.target_file_path, mode='a', newline='') as file:
+        logger.info(f"Writing data to {self.target_file_path}")
+        with open(self.target_file_path, mode='a') as file:
             writer = csv.DictWriter(file, fieldnames=payload.keys())
             if not file_exists:
                 writer.writeheader()
             writer.writerow(payload)
+        logger.info(f"Data written to {self.target_file_path}")
